499e5c3c6b50e85b590a8fbabe9e2634ba265673
durgaraoande <100592850+durgaraoande@users.noreply.github.com>
2022-12-20 14:54:21 +0530

    Add files via upload

---
+++ Newprogram.java
@@ -0,0 +1,11 @@
+class Newprogram
+{
+	public static void main(String args[])
+	{
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter two number");
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println("Sum of two numbers is"+(a+b));
+	}
+}
